2cebf3d7a2f55b447e214d7bbfc1f1c91b63b650
Штагауэр Максим <shtangauer.maxim@mail.ru>
2023-09-14 18:41:07 +0700

    Вывод страницы первой лабораторной».

--- app.py
+++ app.py
@@ -4,5 +4,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-   return "web-сервер на flask"
-    
+   return """
+   <!DOCTYPE html>
+<html>
+    <head>
+        <title>Козицкий Владислав и Штагауэр Максим. Лабораторная 1</title>
+    </head>
+
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Козицкий Владислав и Штагауэр Максим, ФБИ-12, 3 курс 2023
+        </footer>
+    </body>
+</html>
+"""
+
+
+
