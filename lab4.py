de54257ae721bd92c805c766b197a264cf2af310
Штагауэр Максим <shtangauer.maxim@mail.ru>
2023-10-26 18:28:03 +0700

    Автортзация

--- lab4.py
+++ lab4.py
@@ -4,3 +4,20 @@ lab4 = Blueprint('lab4', __name__)
 @lab4.route('/lab4/')
 def lab():
     return render_template('lab4.html')
+
+@lab4.route('/lab4/login', methods = ['GET', 'POST'])
+def login():
+    errors={}
+    username = request.form.get('username')
+    if username == '':
+        errors['username'] = 'не введен логин'
+
+    password = request.form.get('password')
+    if password == '':
+        errors['password'] = 'не введен пароль'
+
+    if username == 'alex' and password == '123':
+        return render_template('successlogin.html', username = username)
+
+    error = 'Неверный логин и/или пароль'
+    return render_template('login.html', error = error, username=username, password=password, errors=errors)
