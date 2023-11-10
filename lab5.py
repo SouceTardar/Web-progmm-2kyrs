c367230cf0906ace746a34369439803a93abf5ff
Штагауэр Максим <shtangauer.maxim@mail.ru>
2023-11-10 16:40:10 +0700

    Страница регистрации

--- lab5.py
+++ lab5.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, Blueprint, redirect
 import psycopg2
 
 
@@ -19,13 +19,8 @@ def dbClose(surcor, connection):
 
 @lab5.route('/lab5/')
 def main():  
-    conn = dbConnect()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM users;")
-    result= cur.fetchall()
-    print(result)
-
-    return render_template('lab5.html')
+    visibleUser = 'Anon'
+    return render_template('lab5.html', username=visibleUser)
 
 @lab5.route('/lab5/users')
 def users():
@@ -36,3 +31,38 @@ def users():
     conn.close()
     return render_template('users.html', users=result)
 
+@lab5.route('/lab5/register', methods = ['GET', 'POST'])
+def regPage():
+    errors = ""
+
+    if request.method == "GET":
+        return render_template('register.html', errors=errors)
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+   
+    if not (username or password):
+        errors = "Пожалуйтса, заполните все поля"
+        print(errors)
+        return render_template("register.html", errors=errors)
+    
+    conn = dbConnect()
+    cur = conn.cursor()
+
+    cur.execute(f"SELECT username FROM users WHERE username = '{username}';")
+
+    if cur.fetchone() is not None:
+        errors = "Пользователь с данным именем уже существует"
+        dbConnect(cur, conn)
+        return render_template('register.html', errors=errors)
+    
+    cur.execute(f"INSERT INTO users (username, password) VALUES ('{username}', '{password}');")
+
+    conn.commit()
+    dbClose(cur, conn)
+
+    return redirect('/lab5/register')
+
+@lab5.route('/lab5/login', methods = ['GET', 'POST'])
+def logPage():
+    return render_template('lab5login.html')
